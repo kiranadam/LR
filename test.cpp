31d5cf4f32f2445f771b62f3eb7c9b6700db4b2b
Kirankumar Adam <kiranadam@gmail.com>
2019-05-09 22:19:40 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
-// https://github.com/wepe/MachineLearning/tree/master/logistic%20regression/use_cpp_and_eigen
-#include <iostream>
-#include <armadillo>
+// https://hackernoon.com/introduction-to-machine-learning-algorithms-logistic-regression-cbdd82d81a36
+
+// g++ lr.cpp test.cpp -o lr -O2 -larmadillo
+
+// ./lr to run
+
+#include "lr.hpp"
 
 using namespace std;
 using namespace arma;
@@ -10,10 +14,25 @@ int main(int, char**)
 
 	mat X;
 	X.load("Iris.csv");
+	int i = X.n_cols-1;
+	vec y = X.col(i);
+	X.shed_col(i);
+	//cout<<y<<endl;
+
+	LR lr;
+
+	lr.train(X,y);
+	vec w = lr.getW();
+
+	//cout<<w<<endl;
+
+	LR lr2;
+
+	lr2.setW(w);
+
+	vec y2 = lr2.predict(X);
 	
-	X.shed_col(X.n_cols-1);
-
-	cout<<X<<endl;
-
+	cout<<y2<<endl;
+	
 	return 0;
 }
